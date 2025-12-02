1a90438050a01b3fa1f0e30ff624ec87f603e653
marcosshc <marcos.heredia@alu.uclm.es>
2025-12-02 12:46:00 +0100

    Implementación completa del Problema 2 (Tarificador)

---
+++ src/main/java/es/uclm/iso2/tarificador/ExcepcionDatosInvalidos.java
@@ -0,0 +1,7 @@
+package es.uclm.iso2.tarificador;
+
+public class ExcepcionDatosInvalidos extends Exception {
+    public ExcepcionDatosInvalidos(String mensaje) {
+        super(mensaje);
+    }
+}
